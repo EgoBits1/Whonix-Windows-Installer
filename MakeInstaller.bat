3bbdafd21935b157bd8fbda7d9268747f8343fa2
Ego <ego-superego-id@protonmail.com>
2018-01-09 03:23:35 +0100

    Added script for auto compile

---
+++ MakeInstaller.bat
@@ -0,0 +1,12 @@
+where /f /r c:\ iscc.exe > location.txt
+@echo off &setlocal
+(for /f "delims=" %%i in (location.txt) do (
+set "line=%%i"
+setlocal enabledelayedexpansion
+set "line=!line:exe"=exe" Whonix.iss!"
+echo(!line!
+endlocal
+))>"location.bat"
+call location.bat
+7za a Installer ./Output/
+copy /b 7za.exe + config.txt + Installer.7z "Install Whonix.exe"
